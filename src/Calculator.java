814d711e78adf309af1e55e59b2b30b8429dec84
Mark Berney <berneydwmark@gmail.com>
2019-12-27 12:56:41 +1300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    private int addition(int a, int b) {
+        return a + b;
+    }
+
+    private int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    private int 
+}
